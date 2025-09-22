a55b600885f13e5c0d42b3ddd4dfb421c35c7220
xiaoxuan010 <20232005046@m.scnu.edu.cn>
2025-09-22 15:06:57 +0800

    feat(pgtbl): print a page table

--- kernel/vm.c
+++ kernel/vm.c
@@ -440,3 +440,31 @@ copyinstr(pagetable_t pagetable, char *dst, uint64 srcva, uint64 max)
     return -1;
   }
 }
+
+void vmprint_rec(pagetable_t pgtbl, int level)
+{
+  for (int i = 0; i < 512; i++)
+  {
+    pte_t pte = pgtbl[i];
+    if (pte & PTE_V)
+    {
+      if (level >= 1)
+        printf("..");
+      for (int l = 1; l < level; l++)
+        printf(" ..");
+      uint64 pa = PTE2PA(pte);
+      printf("%d: pte %p pa %p\n", i, pte, pa);
+      if ((pte & (PTE_R | PTE_W | PTE_X)) == 0)
+      {
+        vmprint_rec((pagetable_t)pa, level + 1);
+      }
+    }
+  }
+}
+
+void vmprint(pagetable_t pgtbl)
+{
+  printf("page table %p\n", pgtbl);
+
+  vmprint_rec(pgtbl, 1);
+}
