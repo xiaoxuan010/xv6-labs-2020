cd949d266b48eb680c68776ec4704f13a29c9bdf
xiaoxuan010 <20232005046@m.scnu.edu.cn>
2025-12-01 14:30:36 +0800

    feat(fs): implement symbolic links

--- kernel/sysfile.c
+++ kernel/sysfile.c
@@ -316,6 +316,41 @@ sys_open(void)
     }
   }
 
+  if (ip->type == T_SYMLINK && !(omode & O_NOFOLLOW))
+  {
+    int depth = 0;
+    while (ip->type == T_SYMLINK)
+    {
+      if (depth >= 10)
+      {
+        iunlockput(ip);
+        end_op();
+        return -1;
+      }
+
+      char target[MAXPATH];
+      int len = readi(ip, 0, (uint64)target, 0, MAXPATH);
+      if (len < 0)
+      {
+        iunlockput(ip);
+        end_op();
+        return -1;
+      }
+      if (len >= MAXPATH)
+        len = MAXPATH - 1;
+      target[len] = 0;
+
+      iunlockput(ip);
+      if ((ip = namei(target)) == 0)
+      {
+        end_op();
+        return -1;
+      }
+      ilock(ip);
+      depth++;
+    }
+  }
+
   if(ip->type == T_DEVICE && (ip->major < 0 || ip->major >= NDEV)){
     iunlockput(ip);
     end_op();
@@ -482,5 +517,33 @@ sys_pipe(void)
     fileclose(wf);
     return -1;
   }
+  return 0;
+}
+
+uint64
+sys_symlink(void)
+{
+  char target[MAXPATH], path[MAXPATH];
+  struct inode *ip;
+
+  if (argstr(0, target, MAXPATH) < 0 || argstr(1, path, MAXPATH) < 0)
+    return -1;
+
+  begin_op();
+  if ((ip = create(path, T_SYMLINK, 0, 0)) == 0)
+  {
+    end_op();
+    return -1;
+  }
+
+  if (writei(ip, 0, (uint64)target, 0, strlen(target)) != strlen(target))
+  {
+    iunlockput(ip);
+    end_op();
+    return -1;
+  }
+
+  iunlockput(ip);
+  end_op();
   return 0;
 }
